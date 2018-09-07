d4dd3837e8d579e5c3151f47dc4a11437d84d352
Arnaud97234 <arnaud.ulric@gmail.com>
2018-09-07 12:24:39 +0200

    add compareJson.js

--- scripts/testSync.sh
+++ scripts/testSync.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
+ledgerLiveVersion=$(cat ../package.json | grep version | cut -d : -f 2 | sed -E 's/.*"([^"]*)".*/\1/g')
 
-if [[ $(uname) == 'Darwin' ]]; then echo 'you are on MacOS'
-  && settingsPath="~/Library/Application Support/Ledger Live/"
-  && app_path="/Applications/Ledger Live.app/Contents/MacOS/Ledger Live"
-elif [[ $(uname) == 'Linux' ]]; then echo 'you are on Linux'
-  && settingsPath="~/.config/Ledger Live"
-  && appPath="~/apps"
+if [[ $(uname) == 'Darwin' ]]; then echo 'you are on MacOS' \
+  && settingsPath="~/Library/Application\ Support/Ledger\ Live/" && echo $settingsPath \
+  && appPath="/Applications/Ledger Live.app/Contents/MacOS/Ledger Live"
+elif [[ $(uname) == 'Linux' ]]; then echo 'you are on Linux' \
+  && settingsPath="~/.config/Ledger Live" \
+  && appPath="~/apps/ledger-live-desktop-$ledgerLiveVersion-linux-x86_64.AppImage"
 else echo 'you are on Windows'
 fi
-  #statements
 
 # clean Ledger Live Application settings directory
 rm -rf $settingsPath
-rm ./src/synced_app.json
+#rm ../src/synced_app.json
 
-# Create new Ledger Live Application settings directory
 # Copy app.json init file for testing
-mkdir $settingsPath
-cp ./src/app.json $settingsPath
+
+cp ../src/app.json ~/Library/Application\ Support/Ledger\ Live/
 
 # Start Ledger Live Desktop app
-$appPath &
+"$appPath" &
 
 # wait for sync
 # kill Ledger Live Desktop process
@@ -30,7 +29,7 @@ kill -9 $lastPid
 echo finished
 
 # Copy app.json file to test folder and compare with reference file
-cp $settings_path/app.json ./src/synced_app.json
+cp $settingsPath/app.json ../src/synced_app.json
 
-./scripts/compare.json
+../scripts/compareJson.js
 # json-diff expected_app.json synced_app.json
