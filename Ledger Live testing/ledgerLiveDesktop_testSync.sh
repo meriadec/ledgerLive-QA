e3ce6e0e00f4798c7995a31abd3e8258ec3e5a93
Arnaud97234 <arnaud.ulric@gmail.com>
2018-09-07 11:06:08 +0200

    update master

--- Ledger Live testing/ledgerLiveDesktop_testSync.sh
+++ Ledger Live testing/ledgerLiveDesktop_testSync.sh
@@ -2,12 +2,12 @@
 
 # clean Ledger Live Application settings directory
 rm -rf ~/Library/Application\ Support/Ledger\ Live/
-rm ~/Workspace/Ledger\ Live\ testing/app_synced.json
+rm ~/Workspace/repos/ledgerLive-QA/Ledger\ Live\ testing/app_synced.json
 
 # Create new Ledger Live Application settings directory
 # Copy app.json init file for testing
 mkdir ~/Library/Application\ Support/Ledger\ Live
-cp ~/Workspace/Ledger\ Live\ testing/app.json ~/Library/Application\ Support/Ledger\ Live
+cp ~/Workspace/repos/ledgerLive-QA/Ledger\ Live\ testing/app.json ~/Library/Application\ Support/Ledger\ Live
 
 # Start Ledger Live Desktop app
 /Applications/Ledger\ Live.app/Contents/MacOS/Ledger\ Live &
@@ -20,6 +20,6 @@ kill -9 $lastPid
 echo finished
 
 # Copy app.json file to test folder and compare with reference file
-cp ~/Library/Application\ Support/Ledger\ Live/app.json ~/Workspace/Ledger\ Live\ testing/app_synced.json
+cp ~/Library/Application\ Support/Ledger\ Live/app.json ~/Workspace/repos/ledgerLive-QA/Ledger\ Live\ testing/app_synced.json
 
 json-diff app_expected.json app_synced.json
